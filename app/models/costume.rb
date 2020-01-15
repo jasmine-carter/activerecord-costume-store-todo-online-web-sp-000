8619d3e483cbd80b407b24be8fcb348e681521a4
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-15 02:59:52 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,6 +1,6 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
 class Costume<ActiveRecord::Base
-  attr_accessor :name
+  attr_accessor :name, :price, :size, :image_url
 
 end
