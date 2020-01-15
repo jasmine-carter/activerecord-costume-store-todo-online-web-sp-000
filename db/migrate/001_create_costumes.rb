bc7878d53da131acf604699bbb3b3f97a7635b6a
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-15 02:57:13 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,11 +3,11 @@ def Costume < ActiveRecord::Migration[5.2]
 
   def change
     create_table :costumes do |t|
-      t.string :name,
-      t.integer :price,
-      t.string :image_url,
-      t.string :size,
-      t.datetime :created_at,
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :size
+      t.datetime :created_at
       t.datetime :updated_at
     end
   end
